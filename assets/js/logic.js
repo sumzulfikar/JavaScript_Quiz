b647bafa181c376649523d7010ef6c37d9200449
sumzulfikar <sumaiya.zulfikar29@gmail.com>
2024-01-12 21:34:47 +0000

    added questions in arrays

---
+++ assets/js/logic.js
@@ -0,0 +1,6 @@
+// TO DO - Create an array in questions.js in a nice structured array   there are nice way to pick question answer and options
+//To Do - Display the questions in the html in colours
+// Add correct display is correct answer is chosen
+// let user move to next question even if answer wrong
+//add timer
+//store scores
---
+++ assets/js/questions.js
@@ -0,0 +1,18 @@
+var quizQuestions=[
+{question:"Inside which HTML element do we put the JavaScript?",
+answer: "<script>",
+option:["<js>", "<scripting>","<javascript>"]
+},
+{question:"The external JavaScript file must contain the <script> tag.e is the correct place to insert a JavaScript in HTML?",
+answer: "The <head> section",
+option:["The <body> section", "Both <head> and <body>"  ,"Doesn't matter!"]
+},
+{question:"How do you create a function in JavaScript?",
+answer: "function myFunction()",
+option:["function: myFunction()", "function= myFunction()"  ,"myFunction()"]
+},
+{question:"How do you call a function named 'myFunction'?",
+answer: "myFunction()",
+option:["call myFunction()", "call function myFunction()"]
+},
+];
